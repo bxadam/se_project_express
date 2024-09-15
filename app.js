fbd2e9934b370ff71f9f1de8dea81edd4742ca76
bxadam <bx.adam03@gmail.com>
2024-09-15 18:55:56 -0400

    (files) create new folders and files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+
 const app = express();
 const { PORT = 3001 } = process.env;
 
@@ -6,6 +7,6 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-//const express = require("express");
-//why does it try to convert the above code from require to import - QF sugg.:
+// const express = require("express");
+// why does it try to convert the above code from require to import - QF sugg.:
 // File is a CommonJS module; it may be converted to an ES module.
