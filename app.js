2986f37cb0e1aab192372a3c07ec89f18eccb24f
bxadam <bx.adam03@gmail.com>
2024-09-16 14:26:32 -0400

    (feat) connect mongoose, validator and add schemas for document creation

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-import express from "express";
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const { PORT = 3001 } = process.env;
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch(console.error);
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-// const express = require("express");
-// why does it try to convert the above code from require to import - QF sugg.:
-// File is a CommonJS module; it may be converted to an ES module.
